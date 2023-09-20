3dea1c5c219de09fc2b0527640a57b296af90bbe
NOTuglyduckling <112571328+NOTuglyduckling@users.noreply.github.com>
2023-09-20 15:05:42 +0200

    dgd

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,7 +1,24 @@
 package personnages;
 
 public class Gaulois {
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
+	public String getNom() {
+		return nom;
+	}
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	public static void main(String[] args) {
+		if (condition) {
+			
+		}
+	}
 }
