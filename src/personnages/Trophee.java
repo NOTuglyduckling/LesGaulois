97c936bd676d4472089920163a69b5b3e7288cf2
NOTuglyduckling <osearle65@gmail.com>
2023-10-02 17:44:33 +0200

    encore

--- src/personnages/Trophee.java
+++ src/personnages/Trophee.java
@@ -1,9 +1,19 @@
 package personnages;
+import personnages.Gaulois;
+import personnages.Equipement;
 
 public class Trophee {
+	private Gaulois gaulois;
+	private Equipement equipement;
+	
 
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
